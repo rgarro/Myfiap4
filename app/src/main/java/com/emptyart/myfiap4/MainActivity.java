c4975e277bbdd9bdecf866bc4c710ecbf907c681
rgarro <rgarro@gmail.com>
2026-05-17 13:23:40 -0600

    we are living in a yellow submarine ...

--- app/src/main/java/com/emptyart/myfiap4/MainActivity.java
+++ app/src/main/java/com/emptyart/myfiap4/MainActivity.java
@@ -3,6 +3,25 @@ package com.emptyart.myfiap4;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ *               |    |    |
+ *              )_)  )_)  )_)
+ *             )___))___))___)\
+ *            )____)____)_____)\\
+ *         _____|____|____|____\\\__
+ * ---------\                   /---------
+ *     ^^^^^ ^^^^^^^^^^^^^^^^^^^^^
+ *     ^^^^      ^^^^     ^^^    ^^
+ *        ^^^^      ^^^*
+ *
+ * Sir Henry Morgan adventures and the Spanish Main
+ * the RESTFUL gunning of Cartagena , the sack of Panama
+ * will C you at Redondo Beach
+ *
+ *
+ *
+ * @author Rolando <rgarro@gmail.com>
+ */
 public class MainActivity extends Activity {
 
     @Override
