2ca40985c3aec0c129a09138a033b1d82e299139
rgarro <rgarro@gmail.com>
2026-05-24 13:03:58 -0600

    si lo ordena el guero palma No hay ninguno que se raje ..

--- app/src/main/java/com/emptyart/myfiap4/MainActivity.java
+++ app/src/main/java/com/emptyart/myfiap4/MainActivity.java
@@ -16,8 +16,14 @@ import android.os.Bundle;
  *
  * Sir Henry Morgan adventures and the Spanish Main
  * the RESTFUL gunning of Cartagena , the sack of Panama
- * will C you at Redondo Beach
- *
+ * El duque de Alba era dueño de una taberna en Coronado
+ * la espada de uno de sus sargentos esta en la Policia Municipal de San Jose
+ * por eso los negros de Jamaica tienen prohibido pasar de Turrialba
+ * En la Guerra de Panama una bateria antiaerea en Leon Cortez disparo, amparandose
+ * a que la Virgen de los Angeles , una mutacion prehistorica los acompañaba
+ * Los operadores del cañon fueron vaporizados y los comandos que bajaron de helicopteros osprey
+ * blindados con electricidad, mataron a los 200 soldados panameños en minutos y la Virgen de Los Angeles no murio
+ * un Lobo de los Rangers la reconocio amiga.
  *
  *
  * @author Rolando <rgarro@gmail.com>
